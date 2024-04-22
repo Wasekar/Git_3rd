b422b2a572881e10c78a675855318d1c4c539ddd
Wasekar <16030187@ycce.in>
2024-04-22 15:48:52 +0530

    new file crear

--- rahul.java
+++ rahul.java
@@ -1,5 +1,10 @@
 public class Git_demo{
     public static void main(String[] args){
         System.out.println("hello to rahul");
+
+        Sytem.out.println("hello");
+
+        System.out.println("hello to diksha");
+
     }
 }
